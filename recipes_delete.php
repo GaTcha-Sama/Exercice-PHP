2b38769e32843964c1bd50eebfa4711fd7a40749
Jeremy Louet <jeremy.louet29@hotmail.fr>
2024-09-18 11:46:27 +0200

    Modif delete

--- recipes_delete.php
+++ recipes_delete.php
@@ -2,6 +2,17 @@
 session_start();
 
 require_once(__DIR__ . '/isConnect.php');
+
+/**
+ * On ne traite pas les super globales provenant de l'utilisateur directement,
+ * ces données doivent être testées et vérifiées.
+ */
+$getData = $_GET;
+
+if (!isset($getData['id']) || !is_numeric($getData['id'])) {
+    echo('Il faut un identifiant pour supprimer la recette.');
+    return;
+}
 ?>
 
 <!DOCTYPE html>
@@ -10,7 +21,7 @@ require_once(__DIR__ . '/isConnect.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Site de Recettes - Ajout de recette</title>
+    <title>Site de Recettes - Supprimer la recette ?</title>
     <link
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
         rel="stylesheet"
@@ -20,20 +31,16 @@ require_once(__DIR__ . '/isConnect.php');
     <div class="container">
 
         <?php require_once(__DIR__ . '/header.php'); ?>
+        <h1>Supprimer la recette ?</h1>
+        <form action="recipes_post_delete.php" method="POST">
+            <div class="mb-3 visually-hidden">
+                <label for="id" class="form-label">Identifiant de la recette</label>
+                <input type="hidden" class="form-control" id="id" name="id" value="<?php echo $getData['id']; ?>">
+            </div>
 
-        <h1>Ajouter une recette</h1>
-        <form action="recipes_post_create.php" method="POST">
-            <div class="mb-3">
-                <label for="title" class="form-label">Titre de la recette</label>
-                <input type="text" class="form-control" id="title" name="title" aria-describedby="title-help">
-                <div id="title-help" class="form-text">Choisissez un titre percutant !</div>
-            </div>
-            <div class="mb-3">
-                <label for="recipe" class="form-label">Description de la recette</label>
-                <textarea class="form-control" placeholder="Seulement du contenu vous appartenant ou libre de droits." id="recipe" name="recipe"></textarea>
-            </div>
-            <button type="submit" class="btn btn-primary">Envoyer</button>
+            <button type="submit" class="btn btn-danger">La suppression est définitive</button>
         </form>
+        <br />
     </div>
 
     <?php require_once(__DIR__ . '/footer.php'); ?>
