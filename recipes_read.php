aa0c1c90ec609fcc24e21f997a1d0d7442354a0b
Jeremy Louet <jeremy.louet29@hotmail.fr>
2024-09-18 12:28:55 +0200

    Comments

--- recipes_read.php
+++ recipes_read.php
@@ -16,15 +16,37 @@ if (!isset($getData['id']) || !is_numeric($getData['id'])) {
 }
 
 // On récupère la recette
-$retrieveRecipeStatement = $mysqlClient->prepare('SELECT r.* FROM recipes r WHERE r.recipe_id = :id ');
-$retrieveRecipeStatement->execute([
+$retrieveRecipeWithCommentsStatement = $mysqlClient->prepare('SELECT r.*, c.comment_id, c.comment, c.user_id, u.full_name FROM recipes r 
+LEFT JOIN comments c on c.recipe_id = r.recipe_id
+LEFT JOIN users u ON u.user_id = c.user_id
+WHERE r.recipe_id = :id ');
+$retrieveRecipeWithCommentsStatement->execute([
     'id' => (int)$getData['id'],
 ]);
-$recipe = $retrieveRecipeStatement->fetch();
+$recipeWithComments = $retrieveRecipeWithCommentsStatement->fetchAll(PDO::FETCH_ASSOC);
 
-if (!$recipe) {
+if ($recipeWithComments === []) {
     echo('La recette n\'existe pas');
     return;
+}
+
+$recipe = [
+    'recipe_id' => $recipeWithComments[0]['recipe_id'],
+    'title' => $recipeWithComments[0]['title'],
+    'recipe' => $recipeWithComments[0]['recipe'],
+    'author' => $recipeWithComments[0]['author'],
+    'comments' => [],
+];
+
+foreach ($recipeWithComments as $comment) {
+    if (!is_null($comment['comment_id'])) {
+        $recipe['comments'][] = [
+            'comment_id' => $comment['comment_id'],
+            'comment' => $comment['comment'],
+            'user_id' => (int) $comment['user_id'],
+            'full_name' => $comment['full_name']
+        ];
+    }
 }
 ?>
 
@@ -53,6 +75,26 @@ if (!$recipe) {
                 <p><i>Contribuée par <?php echo($recipe['author']); ?></i></p>
             </aside>
         </div>
+        <hr />
+        <h2>Commentaires</h2>
+        <?php if ($recipe['comments'] !== []) : ?>
+        <div class="row">
+            <?php foreach ($recipe['comments'] as $comment) : ?>
+                <div class="comment">
+                    <p><?php echo $comment['comment']; ?></p>
+                    <i>(<?php echo $comment['full_name']; ?>)</i>
+                </div>
+            <?php endforeach; ?>
+        </div>
+        <?php else : ?>
+        <div class="row">
+            <p>Aucun commentaire</p>
+        </div>
+        <?php endif; ?>
+        <hr />
+        <?php if (isset($_SESSION['LOGGED_USER'])) : ?>
+            <?php require_once(__DIR__ . '/comments_create.php'); ?>
+        <?php endif; ?>
     </div>
     <?php require_once(__DIR__ . '/footer.php'); ?>
 </body>
